5aa3cc45971058c6ab97787d77f68f9fde63a275
Thekiborg <none>
2023-12-04 22:14:41 +0100

    code update

---
+++ 1.4/Source/ThingClass/GuardingP_StoreSpots.cs
@@ -0,0 +1,52 @@
+﻿using Verse.AI.Group;
+using Verse.Sound;
+
+namespace Thek_GuardingPawns
+{
+    [StaticConstructorOnStartup]
+    public class GuardingP_StoreSpots : Building
+    {
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            StoreThing();
+        }
+
+        public override void DeSpawn(DestroyMode mode = DestroyMode.Vanish)
+        {
+            UnstoreThing();
+            base.DeSpawn(mode);
+        }
+
+        public override void Destroy(DestroyMode mode = DestroyMode.Vanish)
+        {
+            UnstoreThing();
+            base.Destroy(mode);
+        }
+        private void StoreThing()
+        {
+            MapComponent_GuardingPawns mapComp = MapHeld.GetComponent<MapComponent_GuardingPawns>();
+            mapComp.spotsOnMap.Add(this);
+            Log.Message("added");
+            foreach (Building b in mapComp.spotsOnMap)
+            {
+                Log.Message(b.ToString());
+            }
+        }
+
+        private void UnstoreThing()
+        {
+            MapComponent_GuardingPawns mapComp = MapHeld.GetComponent<MapComponent_GuardingPawns>();
+            if (mapComp.spotsOnMap.Contains(this))
+            {
+                MapHeld.GetComponent<MapComponent_GuardingPawns>().spotsOnMap.Remove(this);
+                Log.Message("removed");
+            }
+            foreach (Building b in mapComp.spotsOnMap)
+            {
+                Log.Message(b.ToString());
+            }
+        }
+
+    }
+}
