941fa7698306bcb49ab6df4c2d1e48fc699a8a1a
Thekiborg <none>
2024-04-27 22:28:15 +0200

    Fixed reservation bullshit

--- 1.5/Source/JobDrivers/JobDriver_GuardSpot.cs
+++ 1.5/Source/JobDrivers/JobDriver_GuardSpot.cs
@@ -20,7 +20,7 @@ namespace Thek_GuardingPawns
 
         public override bool TryMakePreToilReservations(bool errorOnFailed)
         {
-            return pawn.ReserveSittableOrSpot(job.targetA.Cell, job);
+            return pawn.Reserve(job.targetA, job);
         }
 
         protected override IEnumerable<Toil> MakeNewToils()
